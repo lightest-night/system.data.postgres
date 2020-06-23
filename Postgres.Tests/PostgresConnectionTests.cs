cfec14d0012aeaa26fb7ee9493029ce26938be0b
James Woodley <james@lightestnight.com>
2020-06-23 14:06:02 +0100

    Differentiate between Unit & Integration Tests
      * Categorise Tests accordingly
      * Separate test runs in CI pipeline
      * Run integration tests against a Postgres Service Container

--- Postgres.Tests/PostgresConnectionTests.cs
+++ Postgres.Tests/PostgresConnectionTests.cs
@@ -1,4 +1,6 @@
-﻿using AutoFixture;
+﻿using System;
+using System.Threading.Tasks;
+using AutoFixture;
 using Microsoft.Extensions.Options;
 using Shouldly;
 using Xunit;
@@ -12,12 +14,28 @@ namespace LightestNight.System.Data.Postgres.Tests
         public PostgresConnectionTests()
         {
             var fixture = new Fixture();
-            var options = fixture.Create<PostgresOptions>();
+            var options = fixture.Build<PostgresOptions>()
+                .Without(o => o.Host)
+                .Without(o => o.Port)
+                .Without(o => o.Pooling)
+                .Without(o => o.Username)
+                .Without(o => o.Password)
+                .Without(o => o.Database)
+                .Do(o =>
+                {
+                    o.Host = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "localhost";
+                    o.Port = Convert.ToInt32(Environment.GetEnvironmentVariable("POSTGRES_PORT") ?? "5432");
+                    o.Pooling = Convert.ToBoolean(Environment.GetEnvironmentVariable("POSTGRES_POOLING") ?? "False");
+                    o.Username = Environment.GetEnvironmentVariable("POSTGRES_USERNAME") ?? "postgres";
+                    o.Password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD") ?? "postgres";
+                    o.Database = Environment.GetEnvironmentVariable("POSTGRES_DATABASE") ?? "postgres";
+                })
+                .Create();
 
             _sut = new PostgresConnection(Options.Create(options));
         }
         
-        [Fact]
+        [Fact, Trait("Category", "Unit")]
         public void ShouldBuildNewConnection()
         {
             // Act
@@ -26,6 +44,16 @@ namespace LightestNight.System.Data.Postgres.Tests
             // Assert
             result.ConnectionString.ShouldNotBeNullOrEmpty();
             result.ConnectionString.ShouldNotBeNullOrWhiteSpace();
+        }
+
+        [Fact, Trait("Category", "Integration")]
+        public void ShouldConnectSuccessfully()
+        {
+            // Act
+            using var result = _sut.Build();
+            
+            // Assert
+            Should.NotThrow(() => result.Open());
         }
     }
 }
